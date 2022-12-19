416a285b9750d6a36e572a2ff59b4dc6ee1742b6
Durgesh Agnihotri <agnihotridurgesh123@gmail.com>
2022-12-19 12:53:38 +0530

    index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://fileshare-study.herokuapp.com";
+const baseURL = "https://file-share-xbkn.onrender.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
